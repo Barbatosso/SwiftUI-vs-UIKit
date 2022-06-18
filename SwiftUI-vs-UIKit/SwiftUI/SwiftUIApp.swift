8e3bc8091c5b9845ed2011be9dd75d895cb393c8
Egor Petrov <epetrov@hey.com>
2022-06-18 23:42:00 +0800

    Add SwiftUI project

---
+++ SwiftUI-vs-UIKit/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by Egor Petrov on 18.06.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
